63ac36e55fbe28324bb3fdb7795622c69cfc7e88
rehfeld13 <rafaelrehfeld97@gmail.com>
2024-02-16 17:49:17 -0300

    Create CategoryButton component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,36 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import Header from "../components/Header";
+import CategotyButton from "../components/CategoryButton";
+import { CATEGORIES } from "@/src/utils/data/products"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [category, setCategoty] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string){
+    setCategoty(selectedCategory)
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Header title="Cardápio" cartQuantityItems={3} />
+      <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategotyButton 
+            title={item} 
+            onPress={() => handleCategorySelect(item)}
+            isSelected={item === category}  
+          />   
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
